67aa6644a8efa318f09af94e2d6e24281bd1f8e0
Justin Moore <jmoore1@live.esu.edu>
2015-01-25 21:37:43 -0500

    Added calc_bitmasks.py

---
+++ calc_bitmasks.py
@@ -0,0 +1,10 @@
+# Remove rightmost black squares
+print hex(0b11101111111011111110111111101111)
+# Remove leftmost black squares
+print hex(0b11110111111101111111011111110111)
+
+# Remove first 4 black squares
+print hex(0b00001111111111111111111111111111)
+
+# Remove last 4 black squares
+print hex(0b11111111111111111111111111110000)
